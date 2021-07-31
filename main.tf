bd3abaca3a60dd97bc1c8970d712553ce00d1084
vzradha <vzradha@gmail.com>
2021-07-31 17:56:37 +1000

    test

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+	region = "ap-southeast-2"
+}
+
+#terraform {
+#	backend "s3" {
+#		bucket = "test-tf-s3-902374903247"
+#		key = "test/terrform.state"
+#		region = "ap-southeast-2"
+#
+#	}
+#}
+resource "aws_s3_bucket" "tf-state" {
+	bucket = "test-tf-s3-902374903247"
+	versioning {
+		enabled = true
+	}
+	server_side_encryption_configuration {
+		rule {
+			apply_server_side_encryption_by_default {
+				sse_algorithm = "AES256"
+			}
+		}
+	}
+}
+
