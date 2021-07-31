c37025312dce193c4a40a8f7ede1c54d1f032b79
vzradha <vzradha@gmail.com>
2021-07-31 17:58:56 +1000

    test1

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ provider "aws" {
 	region = "ap-southeast-2"
 }
 
-#terraform {
-#	backend "s3" {
-#		bucket = "test-tf-s3-902374903247"
-#		key = "test/terrform.state"
-#		region = "ap-southeast-2"
-#
-#	}
-#}
+terraform {
+	backend "s3" {
+		bucket = "test-tf-s3-902374903247"
+		key = "test/terrform.state"
+		region = "ap-southeast-2"
+
+	}
+}
 resource "aws_s3_bucket" "tf-state" {
 	bucket = "test-tf-s3-902374903247"
 	versioning {
